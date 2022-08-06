35d414cb7d6448ff5cee74328de6c49ec464e84f
Rodrigo Bernardes <rsfbernardes@hotmail.com>
2022-08-06 19:37:51 -0300

    first commit

---
+++ src/main/java/br/com/rbernardes/familybudget/FamilyBudgetApplication.java
@@ -0,0 +1,13 @@
+package br.com.rbernardes.familybudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rbernardes/familybudget/FamilyBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rbernardes.familybudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
